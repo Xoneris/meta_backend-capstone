ff4cfff381c7fba7fd1608d3490846295c2a5a2f
Xoneris <kely42@gmx.ch>
2024-02-05 18:24:23 +0100

    Added authentication to the api

--- backend-capstone/littlelemon/restaurant/views.py
+++ backend-capstone/littlelemon/restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from .models import *
 from .serializers import *
 
@@ -10,11 +12,14 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializers
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializers
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
